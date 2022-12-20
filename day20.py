a50748d2a333d6e4cde3a193dbb176c59a8cb849
Caleb Stromberg <calebstromberg@gmail.com>
2022-12-19 21:35:17 -0800

    Complete day 20

---
+++ day20.py
@@ -0,0 +1,38 @@
+import util
+
+data = util.parseFileAs("./inputs/d20.txt", int)
+
+class Node:
+    def __init__(self, val) -> None:
+        self.val = val
+
+    def __repr__(self) -> str:
+        return str(self.val)
+
+out = 0
+indices = []
+vals = []
+for i, val in enumerate(data):
+    n = Node(val)
+    indices.append(n)
+    vals.append(n)
+
+l = len(vals)
+for i, node in enumerate(indices):
+    newI = vals.index(node)
+    n = vals.pop(newI)
+    vals.insert((newI + n.val) % (l - 1), n)
+
+for i, node in enumerate(vals):
+    if node.val == 0:
+        startI = i
+        break
+
+i = startI
+
+out = 0
+for j in range(3):
+    i = (i + 1000) %len(vals)
+    out += vals[i].val
+
+print(out)
